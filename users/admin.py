b1856479ff961b4a1df1afc26eb5457624134865
hyeonsuk <qu3230@gmail.com>
2019-11-13 18:15:41 +0900

    Create user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {"fields": ("avatar", "gender", "bio", "birthdate", "businessman",)},
+        ),
+    )
+
+    list_filter = UserAdmin.list_filter + ("businessman",)
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "gender",
+        "businessman",
+        "is_staff",
+        "is_superuser",
+    )
