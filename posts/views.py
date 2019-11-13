e0abe5ae126e52d9bdcb206c7cbfa542d044a6eb
hyeonsuk <qu3230@gmail.com>
2019-11-14 01:47:30 +0900

    home, detail html update

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,21 @@
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Post
+    paginate_by = 10
+    ordering = "created"
+    paginate_orphans = 5
+    context_object_name = "posts"
+
+
+class PostDetail(DetailView):
+
+    """ PostDetail Definition """
+
+    model = models.Post
