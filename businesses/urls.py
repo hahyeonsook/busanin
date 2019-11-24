bad6b8b5f149b1487f2b65a924a27733c0f683de
hyeonsuk <qu3230@gmail.com>
2019-11-24 10:51:06 +0900

    business 기능 추가

--- businesses/urls.py
+++ businesses/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = "businesses"
 
 urlpatterns = [
-    path("", views.BusinessList.as_view(), name="home"),
-    path("<int:pk>", views.BusinessDetail.as_view(), name="detail"),
+    path("", views.BusinessList.as_view(), name="list"),
+    path("update/", views.UpdateBusinessView.as_view(), name="update"),
+    path("delete/", views.DeleteBusinessView.as_view(), name="delete"),
+    path("<int:pk>/", views.BusinessDetail.as_view(), name="detail"),
 ]
 
--- businesses/views.py
+++ businesses/views.py
@@ -1,13 +1,15 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import DetailView, ListView, UpdateView, DeleteView
 from django.shortcuts import render
-from . import models
+from django.urls import reverse_lazy
+from users import models as user_model
+from . import models as business_model
 
 
 class BusinessList(ListView):
 
     """ BusinessList Definition """
 
-    model = models.Business
+    model = business_model.Business
     paginate_by = 10
     ordering = "created"
     paginate_orphans = 5
@@ -18,4 +20,31 @@ class BusinessDetail(DetailView):
 
     """ BusinessDetail Definition """
 
-    model = models.Business
+    model = business_model.Business
+
+
+class UpdateBusinessView(UpdateView):
+
+    model = business_model.Business
+    template_name = "businesses/business_update.html"
+    fields = [
+        "name",
+        "description",
+        "address",
+        "open_time",
+        "close_time",
+        "phone",
+    ]
+
+    def get_object(self, queryset=None):
+        return self.request
+
+
+class DeleteBusinessView(DeleteView):
+
+    model = business_model.Business
+    template_name = "businesses/business_delete.html"
+    success_url = reverse_lazy("businesses:list")
+
+    def get_object(self, queryset=None):
+        return self.request
