18d2980ca499d110b359f117818d0d0d52daadc4
hyeonsuk <qu3230@gmail.com>
2019-11-24 10:52:53 +0900

    static 파일 생성

--- config/settings.py
+++ config/settings.py
@@ -129,11 +129,14 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 AUTH_USER_MODEL = "users.User"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
+MEDIA_URL = "/media/"
 
-MEDIA_URL = "/media/"# Email Configuration
+# Email Configuration
 EMAIL_HOST = "smtp.mailgun.org"
 EMAIL_PORT = "587"
 EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
