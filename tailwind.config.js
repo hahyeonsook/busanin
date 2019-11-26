d815e2c9e35a35060b66445871f33294c16227b2
hyeonsuk <qu3230@gmail.com>
2019-11-26 18:51:17 +0900

    nav css 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
 module.exports = {
   theme: {
     extend: {
+      spacing: {
+        "72": "18rem",
+        "84": "21rem",
+        "96": "24rem",
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "75vh": "75vh"
+      },
+
+      borderRadius: {
+        x1: "1.5rem"
+      },
+      minHeight: {
+        "50vh": "50vh",
+        "75vh": "75vh"
+      }
+    }
   },
   variants: {},
   plugins: []
