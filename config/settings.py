8247c60313b0a3174de4d52285240787faa144f8
hyeonsuk <qu3230@gmail.com>
2019-11-27 17:23:21 +0900

    debug 조건문 바꿈

--- config/settings.py
+++ config/settings.py
@@ -92,19 +92,19 @@ WSGI_APPLICATION = "config.wsgi.application"
 if DEBUG is False:
     DATABASES = {
         "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
             "ENGINE": "django.db.backends.postgresql",
             "HOST": os.environ.get("RDS_HOST"),
             "NAME": os.environ.get("RDS_NAME"),
             "USER": os.environ.get("RDS_USER"),
             "PASSWORD": os.environ.get("RDS_PASSWORD"),
             "PORT": "5432",
-        }
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
 
