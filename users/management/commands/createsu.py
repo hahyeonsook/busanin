130b040547ec818af65ca2f680f741a3f4f1259f
hyeonsuk <qu3230@gmail.com>
2019-12-01 22:05:53 +0900

    update container commands

--- users/management/commands/createsu.py
+++ users/management/commands/createsu.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
     help = "This command creates superuser"
 
     def handle(self, *args, **options):
-        admin = User.objects.get(username="ebadmin", None)
+        admin = User.objects.get(username="ebadmin")
         if not admin:
             User.objects.create_superuser("ebadmin", "qu3230@gmail.com", "123456")
             self.stdout.write(self.style.SUCCESS(f"Superuser created!"))
