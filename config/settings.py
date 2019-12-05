6a8e30d5d4379c37d4045e7fae86b1f81a0323af
hyeonsook95 <qu3230@gmail.com>
2019-12-06 01:25:23 +0900

    login/signup link 변경

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+
+DEBUG = bool(os.environ.get("DEBUG"))
+
 
 ALLOWED_HOSTS = ["busanin.be", ".elasticbeanstalk.com", "127.0.0.1"]
 
--- users/views.py
+++ users/views.py
@@ -82,7 +82,7 @@ def complete_verification(request, key):
 # 1. Request a user's Github identity
 def github_login(request):
     client_id = os.environ.get("GH_ID")
-    redirect_uri = "http://127.0.0.1:8000/users/login/github/callback"
+    redirect_uri = "https://busanin.be/users/login/github/callback"
     return redirect(
         f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user",
     )
@@ -158,7 +158,7 @@ def github_callback(request):
 
 def kakao_login(request):
     client_id = os.environ.get("KAKAO_ID")
-    redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
+    redirect_uri = "https://busanin.be/users/login/kakao/callback"
     return redirect(
         f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
     )
@@ -172,7 +172,7 @@ def kakao_callback(request):
     try:
         code = request.GET.get("code", None)
         client_id = os.environ.get("KAKAO_ID")
-        redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
+        redirect_uri = "https://busanin.be/users/login/kakao/callback"
         token_request = requests.get(
             f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={redirect_uri}&code={code}"
         )
