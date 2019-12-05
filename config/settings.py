d485e715cfac7f8294c549561efedff09c47981f
hyeonsook95 <qu3230@gmail.com>
2019-12-05 16:07:22 +0900

    test

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG"))
+DEBUG = False
 
 ALLOWED_HOSTS = [".elasticbeanstalk.com", "127.0.0.1"]
 
