f4dcd144a06e828c0ae7d0dbe5a8049943dbcddc
hyeonsook95 <qu3230@gmail.com>
2019-12-05 18:27:06 +0900

    domain

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".elasticbeanstalk.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["busanin.be", ".elasticbeanstalk.com", "127.0.0.1"]
 
 
 # Application definition
