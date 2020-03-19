96a376c37861f6b886e02df57b5d0967d9805c16
hyeonsook95 <qu3230@gmail.com>
2020-03-20 06:50:38 +0900

    mixins의 주석을 좀 더 명확하게 바꿈 "

--- core/mixins.py
+++ core/mixins.py
@@ -6,7 +6,7 @@ from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 
 class EmailLoginOnlyMixin(UserPassesTestMixin):
 
-    """ 사용자가 Email User가 아닐 때 보여주는 View"""
+    """ 사용자가 Email User가 아닐 때 보여주는 View로 제한하는 Mixin"""
 
     def test_func(self):
         return not self.request.user.login_method == "email"
@@ -18,7 +18,7 @@ class EmailLoginOnlyMixin(UserPassesTestMixin):
 
 class LoggedOutOnlyMixin(UserPassesTestMixin):
 
-    """ 사용자가 Log out 상태일 때 보여주는 View"""
+    """ 사용자가 Log out 상태일 때 보여주는 View로 제한하는 Mixin"""
 
     def test_func(self):
         return not self.request.user.is_authenticated
@@ -31,14 +31,14 @@ class LoggedOutOnlyMixin(UserPassesTestMixin):
 
 class LoggedInOnlyMixin(LoginRequiredMixin):
 
-    """ 사용자가 Log in 상태가 아닐 때, log in으로 넘겨주는 View """
+    """ 사용자가 Log in 상태가 아닐 때, log in으로 넘겨주는 View로 만드는 Mixin """
 
     login_url = reverse_lazy("users:login")
 
 
 class NotFormSuceesMessageMixin:
 
-    """ Form이 아닌 View들에서 SuccessMessage를 추가해주는 View """
+    """ Form이 아닌 View들에서 SuccessMessage를 추가해주는 View로 만드는 Mixin """
 
     success_message = ""
 
